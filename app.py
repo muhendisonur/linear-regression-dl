fb7b6d1dc8548b897c21af26b48e1d04837df42c
Onur Yildiz <muhendisonuryildiz@gmail.com>
2025-02-21 09:59:27 +0300

    first push

---
+++ app.py
@@ -0,0 +1,31 @@
+import torch
+
+# f = w * x + b
+# f = 5 * x
+
+X = torch.randn(5, dtype=torch.float16)
+Y = X * 5
+
+w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
+
+def forward(x):
+    return x * w
+
+def loss(y, y_pred):
+    return ((y - y_pred)**2).mean() # MSE
+
+n_epoch = 200
+lr = 0.01
+
+for epoch in range(n_epoch):
+    y_pred = forward(X)
+    l = loss(Y, y_pred)
+    l.backward()
+
+    with torch.no_grad():
+        w -= lr * w.grad
+    w.grad.zero_()
+    if((epoch+1) % 10 == 0):
+        print(f"Epoch: {epoch+1}, loss: {l.item():.2f}")
+
+print(f"Y: {Y} \nY_pred: {forward(X)}")
